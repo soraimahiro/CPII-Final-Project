e6babb4a5e25dcb4c73ef3706ef6d65aa2a950c1
soraimahiro <cherngci96@gmail.com>
2025-05-17 16:43:25 +0800

    complete vector.c

--- vector.c
+++ vector.c
@@ -1,7 +1,43 @@
 #include "vector.h"
 
-vector initVector() {}
-void pushbackVector(vector* vec, int32_t val) {}
-void popbackVector(vector* vec) {}
-void clearVector(vector* vec) {}
-void eraseVector(vector* vec, int index) {}
+vector initVector() {
+    vector vec;
+    vec.SIZE = 0;
+    memset(vec.array, 0, sizeof(vec.array));
+    return vec;
+}
+
+void pushbackVector(vector* vec, int32_t val) {
+    if (vec == NULL || vec->SIZE >= 256) {
+        return;
+    }
+    vec->array[vec->SIZE] = val;
+    vec->SIZE++;
+}
+
+void popbackVector(vector* vec) {
+    if (vec == NULL || vec->SIZE == 0) {
+        return;
+    }
+    vec->SIZE--;
+    vec->array[vec->SIZE] = 0;
+}
+
+void clearVector(vector* vec) {
+    if (vec == NULL) {
+        return;
+    }
+    vec->SIZE = 0;
+    memset(vec->array, 0, sizeof(vec->array));
+}
+
+void eraseVector(vector* vec, int index) {
+    if (vec == NULL || index < 0 || index >= vec->SIZE) {
+        return;
+    }
+    for (uint32_t i = index; i < vec->SIZE - 1; i++) {
+        vec->array[i] = vec->array[i + 1];
+    }
+    vec->SIZE--;
+    vec->array[vec->SIZE] = 0;
+}
